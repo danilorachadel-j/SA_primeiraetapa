da26dff75014c1d6bda6dc13d82cc7a36368519d
Danilorachadel-j <danilo_rachadel-j@>
2026-05-05 09:28:52 -0300

    pull - inicializei o script

---
+++ SCRIPTS/script-cadastro.js
@@ -0,0 +1,11 @@
+document.addEventListener('DOMContentLoaded', () =>) {
+     const form = document.querySelector('form');
+    const inputCpf = document.getElementById('cpf');
+    const inputTelefone = document.getElementById('telefone');
+    const inputCep = document.getElementById('cep');
+    const senha = document.getElementById('senha');
+    const confirmaSenha = document.getElementById('confirmaSenha');
+
+    
+}
+
