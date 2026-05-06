67477c5a9b16ca1fb84f2b24cb4f25214e247a80
Danilorachadel-j <danilo_rachadel-j@>
2026-05-06 09:56:31 -0300

    pull - finalizei o script da tela de link para o cadastro

--- SCRIPTS/script-inicio-cadastro.js
+++ SCRIPTS/script-inicio-cadastro.js
@@ -1,2 +1,13 @@
-document.addEventListener
+document.addEventListener('DOMContentLoaded',() => {
+    const btn = document.getElementById('betn-cadastro');
+
+    btn.addEventListener('click', () =>{
+        btn.innerText = "Carregando...";
+    btn.style.opacity = "0.7";
+
+    setTimeout(() => {
+        alert("Iniciando fluxo de cadastro!");
+    },1000);
+});
+});
 
