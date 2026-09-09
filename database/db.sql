a5b09ac940c45f118677f93b60d6acf9820ccc3b
Danilorachadel-j <danilo_rachadel-j@>
2026-09-09 08:15:33 -0300

    add - finalizei o banco de dados

--- database/db.sql
+++ database/db.sql
@@ -1,4 +1,19 @@
 CREATE DATABASE sa_ferrorama_d_s_i_n;
 USE sa_ferrorama_d_s_i_n;
 
-CREATE TABLE 
+CREATE TABLE adm(
+id INT AUTO_INCREMENT PRIMARY KEY,
+nome VARCHAR(200) NOT NULL,
+email VARCHAR(75) NOT NULL
+);
+
+CREATE TABLE trem(
+  id INTO AUTO_INCREMENT PRIMARY KEY,
+  id_adm INT NOT NULL,
+  descricao TEXT NOT NULL,
+  tipo VARCHAR(200) NOT NULL,
+  prioridade ENUM('baixa', 'media', 'alta') NOT NULL,
+  data_cadastro DATE NOT NULL,
+  FOREIGN KEY (id_adm) REFERENCES adm(id) ON DELETE CASCADE
+);
+
