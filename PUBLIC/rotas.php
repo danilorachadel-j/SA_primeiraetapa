22d649b580993bc78a257c333c33d27b67283763
SamuelUrdaneta007 <samuel_urdaneta@>
2026-09-17 08:45:41 -0300

    add- arrumei um pocuo do css

--- PUBLIC/rotas.php
+++ PUBLIC/rotas.php
@@ -5,17 +5,17 @@
     <title>Rotas</title>
     <link rel="stylesheet" href="../assets/style/style.css">
 </head>
-<body>
+<body id="body-r">
     
     <div class="rotas">
 
         <div class="titulo-rotas">
             <h1>Rotas de trem em diferentes regiões do Brasil</h1>
         </div>
-        <div class="rota-rj">
-            
-        </div>
         <div class="rota-df">
+            <img src="../assets/img/rota-brasilia.png" alt="">
+        </div>
+        <div class="rota-rj">
 
         </div>
     </div>
