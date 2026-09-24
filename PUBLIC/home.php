48d30d0b4f03fa11471b02bec09a959335cb20a8
nicolasmodesti-cmyk <nicolas_modesti@estuante.sesisenai.org.br>
2026-09-24 08:58:46 -0300

    add - adcionei a tela de home_adm e coloquei seus locais de acessos lá como verificar ou cadastrar sensores

--- PUBLIC/home.php
+++ PUBLIC/home.php
@@ -60,30 +60,9 @@
                 </div>
             </div>
         </div>
-        <div class="part-2">
-            <div class="titulo2">
-                <h1>Opções</h1>
-            </div>
-            <div class="ops">
-                <div class="op1">
-                    <a href="cadastro.php">
-                        <h1>Cadastrar sensor</h1>
-                    </a>
-                </div>
-                <div class="op2">
-                    <a href="verificar_sensores.php">
-                    <h1>Verificar sensores</h1>
-                    </a>
-                </div>
-            </div>
-        </div>
 
     </main>
-    <footer>
-        <div class="embaixo">
-            <img src="../assets/img/Logo.png" alt="">
-        </div>
-    </footer>
+    
 </body>
 
 </html>
